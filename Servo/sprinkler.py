63cb484e39ca09d4be3bdd68988540e628ba7199
mstcyr2 <98985878+mstcyr2@users.noreply.github.com>
2022-04-21 10:58:04 -0400

    Update sprinkler.py

--- Servo/sprinkler.py
+++ Servo/sprinkler.py
@@ -11,18 +11,13 @@ servo = Servo(12, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factor
 go = True
 
 while go:
-    for i in range(0, 90):
+    for i in range(90, 270):
         servo.value = math.sin(math.radians(i))
         sleep(0.01)
-    for i in range(90, 180):
+    for i in range(270, 450):
         servo.value = math.sin(math.radians(i))
         sleep(0.1)
-    for i in range(180, 270):
-        servo.value = math.sin(math.radians(i))
-        sleep(0.01)
-    for i in range(270, 360):
-        servo.value = math.sin(math.radians(i))
-        sleep(0.1)
+        i = i + 30
     
         
         
